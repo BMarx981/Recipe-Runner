e2b8d3acae5b27273794a869f691deb22cf60679
BMarx981 <b.marx981@gmail.com>
2020-04-13 14:13:08 -0400

    "Adjust colors in main Screen"

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const Color mainTheme = Color(0xff992233);
-const List<Color> colors = [Color(0xff2211ff), Color(0xffBB4455)];
-const List<double> stops = [0.0, 0.3];
+const List<Color> colors = [Color(0xff886666), Color(0xffBB4455)];
+const List<double> stops = [0.0, 0.5];
 const LinearGradient colorGrad = LinearGradient(colors: colors, stops: stops);
 const Color white = Colors.white;
