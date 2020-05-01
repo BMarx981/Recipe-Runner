8b229bc345fa1fac2fae0d68940d1f29efb50062
BMarx981 <b.marx981@gmail.com>
2020-04-30 23:17:01 -0400

    "Add title and rando text to edit screen"

--- lib/screens/edit_recipe_screen.dart
+++ lib/screens/edit_recipe_screen.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_writer/models/recipe.dart';
+import 'package:recipe_writer/utils/colors.dart';
 
 class EditRecipeScreen extends StatelessWidget {
+  final Recipe recipe;
+  EditRecipeScreen({this.recipe});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          child: Text('Edit this shit!!'),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: false,
+          elevation: 0.0,
+          backgroundColor: red,
+          title: Text(
+            'Edit',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 34,
+            ),
+          ),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(gradient: colorGrad),
+                child: Text('Enter some text here.'),
+              ),
+            ),
+          ],
         ),
       ),
     );
