b8d34603a4e711aab0b0a5bc607c08c06a5042bc
BMarx981 <b.marx981@gmail.com>
2020-05-10 14:59:27 -0400

    "Upgraded Flutter and used debugPrint in networking"

--- lib/utils/networking.dart
+++ lib/utils/networking.dart
@@ -24,6 +24,7 @@ class Networking {
 //    http.Response response = await http.get(apiCall);
     http.Response response = await http.get('http://127.0.0.1:8080/second');
     if (response.statusCode >= 200 && response.statusCode < 300) {
+      debugPrint(response.body, wrapWidth: 1000);
       return json.decode(response.body);
     }
     return null;
@@ -34,7 +35,7 @@ class Networking {
 //    http.Response response = await http.get(apiCall);
     http.Response response = await http.get('http://127.0.0.1:8080/third');
     if (response.statusCode >= 200 && response.statusCode < 300) {
-//      debugPrint(response.body, wrapWidth: 1000);
+      debugPrint(response.body, wrapWidth: 1000);
       return json.decode(response.body);
     }
     print('Problem with the response is ${response.statusCode}');
