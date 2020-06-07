35f8bdbf9a137a9ccb19b8b5ac3ae14c75faea81
BMarx981 <b.marx981@gmail.com>
2020-06-06 20:56:58 -0400

    "Add dependencies for plugins in pubspec.yaml file"

---
+++ lib/screens/camera_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CameraScreen extends StatefulWidget {
+  CameraScreen({Key key}) : super(key: key);
+
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Camera Screen'),
+      ),
+      body: Container(
+        child: Text('Camera button'),
+      ),
+    );
+  }
+}
