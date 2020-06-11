81e62f67c73d3b69fbb5e8fc63a6994266eaac6c
BMarx981 <b.marx981@gmail.com>
2020-06-11 14:29:45 -0400

    "Add cameraScreen Class and all functionality"

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -1,6 +1,7 @@
-import 'package:camera/camera.dart' as cam;
-import 'package:camera/new/camera.dart';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 class CameraScreen extends StatefulWidget {
   CameraScreen({Key key}) : super(key: key);
@@ -10,7 +11,7 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
-  cam.CameraController controller;
+  CameraController controller;
   List cameras;
   int selectedCameraIndex;
   String imagePath;
@@ -18,8 +19,9 @@ class _CameraScreenState extends State<CameraScreen> {
   @override
   void initState() {
     super.initState();
-    cam.availableCameras().then((availableCameras) {
+    availableCameras().then((availableCameras) {
       cameras = availableCameras;
+
       if (cameras.length > 0) {
         setState(() {
           selectedCameraIndex = 0;
@@ -27,51 +29,144 @@ class _CameraScreenState extends State<CameraScreen> {
 
         _initCameraController(cameras[selectedCameraIndex]).then((void v) {});
       } else {
-        print('No camera available');
+        print("No camera available");
       }
     }).catchError((err) {
-      print('Errors: ${err.code} \n Error Message: ${err.message}');
+      print('Error: $err.code\nError Message: $err.message');
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Camera Screen'),
-      ),
-      body: Container(
-        child: Text('Camera button'),
-      ),
+        appBar: AppBar(
+          title: Text('Camera Screen'),
+        ),
+        body: Container(
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: _cameraPreviewWidget(),
+                ),
+                SizedBox(height: 10.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    _cameraTogglesRowWidget(),
+                    _captureControlRowWidget(context),
+                    Spacer()
+                  ],
+                ),
+                SizedBox(height: 20.0)
+              ],
+            ),
+          ),
+        ));
+  }
+
+  Widget _cameraPreviewWidget() {
+    if (controller == null || !controller.value.isInitialized) {
+      return const Text(
+        'Loading',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20.0,
+          fontWeight: FontWeight.w900,
+        ),
+      );
+    }
+
+    return AspectRatio(
+      aspectRatio: controller.value.aspectRatio,
+      child: CameraPreview(controller),
     );
   }
 
-  Future _initCameraController(camera) async {
+  Future _initCameraController(CameraDescription cameraDescription) async {
     if (controller != null) {
       await controller.dispose();
     }
 
-    controller =
-        cam.CameraController(cameraDescription, cam.ResolutionPreset.high);
+    controller = CameraController(cameraDescription, ResolutionPreset.high);
 
+    // If the controller is updated then update the UI.
     controller.addListener(() {
       if (mounted) {
         setState(() {});
       }
 
-      if (controller.value.hashError) {
+      if (controller.value.hasError) {
         print('Camera error ${controller.value.errorDescription}');
       }
     });
 
     try {
       await controller.initialize();
-    } on cam.CameraException catch (e) {
+    } on CameraException catch (e) {
       _showCameraException(e);
     }
 
     if (mounted) setState(() {});
   }
 
-  void _showCameraException(cam.CameraException e) {}
+  void _showCameraException(CameraException e) {
+    String errorText = 'Error: ${e.code}\nError Message: ${e.description}';
+    print(errorText);
+
+    print('Error: ${e.code}\n${e.description}');
+  }
+
+  _cameraTogglesRowWidget() {}
+
+  _captureControlRowWidget(BuildContext context) {}
+
+  Widget _captureControlRowWidget(context) {
+    return Expanded(
+      child: Align(
+        alignment: Alignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            FloatingActionButton(
+                child: Icon(Icons.camera),
+                backgroundColor: Colors.blueGrey,
+                onPressed: () {
+                  _onCapturePressed(context);
+                })
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onCapturePressed(context) async {
+    // Take the Picture in a try / catch block. If anything goes wrong,
+    // catch the error.
+    // try {
+    //   // Attempt to take a picture and log where it's been saved
+    //   final path = join(
+    //     // In this example, store the picture in the temp directory. Find
+    //     // the temp directory using the `path_provider` plugin.
+    //     (await getTemporaryDirectory()).path,
+    //     '${DateTime.now()}.png',
+    //   );
+    //   print(path);
+    //   await controller.takePicture(path);
+
+    //   // If the picture was taken, display it on a new screen
+    //   Navigator.push(
+    //     context,
+    //     MaterialPageRoute(
+    //       builder: (context) => PreviewImageScreen(imagePath: path),
+    //     ),
+    //   );
+    // } catch (e) {
+    //   // If an error occurs, log the error to the console.
+    //   print(e);
+    // }
+  }
 }
