f7c6a7abb22c55d08ddad81f4f8eae92e0e08e89
BMarx981 <b.marx981@gmail.com>
2020-08-06 22:51:30 -0400

    Add id to Event class

--- lib/models/event.dart
+++ lib/models/event.dart
@@ -1,8 +1,9 @@
 class Event {
+  final int id;
   final DateTime date;
   final Map<String, List<String>> recipesOnDate;
 
-  Event({this.date, this.recipesOnDate});
+  Event({this.id, this.date, this.recipesOnDate});
 
   // Map<String, dynamic> toMap() {
   //   String listString = '';
