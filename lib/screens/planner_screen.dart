22f985392ac132f967540ff5016663d66f4ffa2b
BMarx981 <b.marx981@gmail.com>
2020-08-10 14:58:12 -0400

    "Separate out add event modal. Add close button and autofocus to text field"

--- lib/screens/planner_screen.dart
+++ lib/screens/planner_screen.dart
@@ -79,107 +79,162 @@ class _PlannerScreenState extends State<PlannerScreen> {
           height: 18,
         ),
         Center(
-          child: RaisedButton(
-            child: Text('Add'),
+          child: FlatButton(
+            child: Container(
+              width: 100,
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(35),
+                color: red,
+              ),
+              child: Center(
+                child: Text(
+                  'Add',
+                  style: TextStyle(
+                    color: white,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
             onPressed: () {
-              TextEditingController controller = TextEditingController();
-
-              showModalBottomSheet(
-                  backgroundColor: Color(0x00FFFF54),
-                  context: context,
-                  builder: (context) {
-                    return Container(
-                      padding: EdgeInsets.all(18),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(35),
-                        color: red,
-                      ),
-                      child: Column(
-                        children: <Widget>[
-                          TextField(
-                            cursorColor: white,
-                            decoration: InputDecoration(
-                              hintStyle: TextStyle(
-                                color: white,
-                              ),
-                              fillColor: white,
-                              focusColor: white,
-                              enabledBorder: UnderlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: white,
-                                ),
-                              ),
-                              hintText: 'Enter a recipe',
-                            ),
-                            controller: controller,
-                          ),
-                          SizedBox(height: 20),
-                          FlatButton(
-                            onPressed: () {
-                              addEvent(_selectedDay, controller.text);
-                              setState(() {});
-                              Navigator.pop(context);
-                            },
-                            child: Container(
-                              height: 50,
-                              width: 100,
-                              decoration: BoxDecoration(
-                                color: Colors.amber,
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(35),
-                                ),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  'Submit',
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  });
+              modalSheet(context);
             },
           ),
         ),
         SizedBox(
-          height: 18,
+          height: 12,
         ),
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: (_selectedEvents.length == 0)
               ? SizedBox(height: 1)
-              : Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: white),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ListView.separated(
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) {
-                        return Center(
-                          child: Container(
-                            child: Text(
-                              _selectedEvents[index]['name'].toString(),
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: white,
-                              ),
-                            ),
-                          ),
-                        );
-                      },
-                      separatorBuilder: (context, index) => Divider(
-                        color: Colors.amberAccent,
-                      ),
-                      itemCount: _selectedEvents.length,
-                    ),
-                  ),
-                ),
+              : buildListContainer(),
         )
       ],
     );
+  }
+
+  Container buildListContainer() {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: white),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.separated(
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            return Dismissible(
+              key: Key(
+                  index.toString() + _selectedEvents[index]['name'].toString()),
+              onDismissed: (direction) {
+                // Remove the item from the data source.
+                setState(() {
+                  _selectedEvents.removeAt(index);
+                });
+                Scaffold.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                        "${_selectedEvents[index]['name'].toString()} dismissed"),
+                  ),
+                );
+              },
+              child: Center(
+                child: Container(
+                  child: Text(
+                    _selectedEvents[index]['name'].toString(),
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: white,
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+          separatorBuilder: (context, index) => Divider(
+            color: Colors.amberAccent,
+          ),
+          itemCount: _selectedEvents.length,
+        ),
+      ),
+    );
+  }
+
+  void modalSheet(BuildContext context) {
+    TextEditingController controller = TextEditingController();
+    showModalBottomSheet(
+        backgroundColor: Color(0x00FFFF54),
+        context: context,
+        builder: (context) {
+          return Container(
+            padding: EdgeInsets.all(18),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(35),
+              color: red,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    FlatButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: Icon(
+                        Icons.close,
+                        color: white,
+                      ),
+                    )
+                  ],
+                ),
+                TextField(
+                  autofocus: true,
+                  style: TextStyle(
+                    color: white,
+                  ),
+                  cursorColor: white,
+                  decoration: InputDecoration(
+                    hintStyle: TextStyle(
+                      color: white,
+                    ),
+                    fillColor: white,
+                    focusColor: white,
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: white,
+                      ),
+                    ),
+                    hintText: 'Enter a recipe',
+                  ),
+                  controller: controller,
+                ),
+                SizedBox(height: 20),
+                FlatButton(
+                  onPressed: () {
+                    addEvent(_selectedDay, controller.text);
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    height: 50,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(35),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Submit',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
